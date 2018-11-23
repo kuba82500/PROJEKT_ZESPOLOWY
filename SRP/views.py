942ee5553ad91b1d15c4145e1e0dd938aeb4f8f7
kuba82500 <kuba82500@gmail.com>
2018-11-23 02:54:22 +0100

    Add files via upload

--- SRP/views.py
+++ SRP/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 
 def main_page(request):
@@ -14,5 +15,17 @@ def login(request):
 @login_required()
 def account_user(request):
     return render(request, 'profile.html')
+
+
+def signup_view(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(main_page)
+        else:
+            form = RegisterForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
