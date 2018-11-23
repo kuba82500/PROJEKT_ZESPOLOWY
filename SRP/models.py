f36910e1585f9fe5444bc13ff71d30450fddc361
kuba82500 <kuba82500@gmail.com>
2018-11-23 02:53:17 +0100

    Add files via upload

--- SRP/models.py
+++ SRP/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 
 class Firma(models.Model):
+    id_firma = models.AutoField(primary_key=True, default=1)
     nazwaFirmy = models.CharField(max_length=45, name="NazwaFirmy")
     haslo = models.CharField(max_length=32, name="haslo")
-    email = models.CharField(max_length=45, name="email")
+    email = models.EmailField()
     czyZaakceptowano = models.BooleanField()
 
 
 class Praktyki(models.Model):
+    id_Praktyki = models.AutoField(primary_key=True, default=1)
     iloscMiejscMax = models.IntegerField(name="iloscMiejscMax")
     iloscMiejscZajetych = models.IntegerField(name="iloscMiejscZajetych")
     dataUtworzenia = models.DateTimeField(name="DataUtworzenia")
+
     dataRozpoczecia = models.DateTimeField(name="DataRozpoczecia")
     dataZakonczenia = models.DateTimeField(name="DataZakonczenia")
     czyPlatne = models.BooleanField()
@@ -19,10 +22,11 @@ class Praktyki(models.Model):
     stanowisko = models.CharField(max_length=45, name="Stanowisko")
     opis = models.TextField(max_length=255, name="opis")
     miasto = models.CharField(max_length=45, name="miasto")
-    #id_Firma = models.ForeignKey(Firma, on_delete=models.CASCADE)
+    #id_Firma = models.ForeignKey(Firma, on_delete=models.SET_NULL())
 
 
 class Grupa(models.Model):
+    id_Grupy = models.AutoField(primary_key=True, default=1)
     imieUczestnika = models.CharField(max_length=45, name="imieUczestnika")
     nazwiskoUczestnika = models.CharField(max_length=45, name="nazwiskoUczestnika")
     imieProwadzacego = models.CharField(max_length=45, name="imieProwadzacego")
@@ -32,14 +36,15 @@ class Grupa(models.Model):
 
 
 class Uzytkownik(models.Model):
+    id_Uzytkownika = models.AutoField(primary_key=True, default=1)
     imie = models.CharField(max_length=45, name="imie")
     nazwisko = models.CharField(max_length=45, name="nazwisko")
-    email = models.CharField(max_length=45, name="email")
+    email = models.EmailField()
     haslo = models.CharField(max_length=45, name="haslo")
     nrIndex = models.CharField(max_length=45, name="numerIndeksu")
-    dataUtworzenia = models.DateTimeField()
-    dataEdycji = models.DateTimeField()
-    czyAdmin = models.BooleanField()
+    #dataUtworzenia = models.DateTimeField()
+    #dataEdycji = models.DateTimeField()
+    #czyAdmin = models.BooleanField()
     #id_Grupa = models.ForeignKey(Grupa, on_delete=models.CASCADE)
     #id_Praktyki = models.ForeignKey(Praktyki, on_delete=models.CASCADE)
     #id_Firma = models.ForeignKey(Firma, on_delete=models.CASCADE)
